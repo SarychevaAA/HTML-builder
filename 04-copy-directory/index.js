95d4e6331dcc54712db175c62cf373baecd0d1f7
Aleksandra <alex.sarychewa2014@yandex.ru>
2023-05-09 01:55:28 +0300

    feat: add solution to task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const {readdir, mkdir, copyFile, unlink} = require('node:fs/promises');
+const path = require('path');
 
+mkdir(path.resolve(__dirname, 'files-copy'), {recursive: true}).then(() =>
+    {
+        readdir(path.resolve(__dirname, 'files-copy'), {withFileTypes:true}).then((files)=>{
+            files.map(file => unlink(path.resolve(path.resolve(__dirname, 'files-copy'), file.name)));
+        }).then(()=>{
+            readdir(path.resolve(__dirname, 'files'), {withFileTypes:true}).then(files=>{
+                for (const file of files){
+                    copyFile(path.resolve(__dirname, 'files', file.name), path.resolve(__dirname, 'files-copy', file.name)).then(() => {});
+                }
+            })
+        })
+    }
+
+
+)
